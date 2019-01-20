cfd8884ca8823d42b7069a6be8e72dbc4bd99294
Ryan Glazer <rglazer003@gmail.com>
2019-01-20 17:54:23 -0600

    Starting WotSL, stats and items vars set up

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.lang.reflect.Array;
+// TODO: Write equipment list
+// TODO: Write spell list
+// TODO: Write skill list
+// TODO: Review potions after core gameplay is finished
+// TODO: Combat methods
+// TODO: Story
+// TODO: Side stuff and merchants
+// TODO: Random encounter methods
+// TODO: Figure out level up and basic stat balance
+public class Main {
+    public static void main(String[] args) {
+        String playerName = "";
+        int gold = 0;
+        int strength = 5;
+        int intelligence = 5;
+        int agility = 5;
+        int luck = 1;
+        int defence = 10;
+        int attack = 10;
+        // Weapon, Armor, Accessory, Helm
+        // Write equipment list
+        int [] equipment = {0,0,0,0};
+        //Spell list
+        // Write Spell list
+        int [] spellList = {0,0,0,0,0,0,0,0,0,0};
+        // Skill list
+        // Write skill list
+        int [] skillList = {0,0,0,0,0,0,0,0,0,0};
+        //Potions
+        // HP, MP, Antidote, Holy Water, Fire Flask
+        // Maybe make more potions, if I implement more involved resistances add more damage potions
+        int [] potions = {0,0,0,0,0};
+        int [] questStuff;
+
+    }
+}
